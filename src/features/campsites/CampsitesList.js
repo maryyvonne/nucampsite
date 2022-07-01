170b4c804e60c6ea53218f849de293df915253ac
Mary Varnado <59319147+maryyvonne@users.noreply.github.com>
2022-07-01 11:53:48 -0500

    CampsiteDirectoryPage

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,14 +1,16 @@
+import { useSelector } from 'react-redux';
 import { Col, Row, } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = () => {
-    const campsites = selectAllCampsites();
+const CampsitesList = ({ setCampsiteId }) => {
+    const campsites = useSelector(selectAllCampsites);
+    console.log('campsites:', campsites);
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' className='m-4' key={campsite.id}>
+                    <Col md='5' className='m-4' key={campsite.id} onClick={() => setCampsiteId(campsite.id)}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,12 +1,17 @@
 import { Col, Row } from 'reactstrap';
-
+import { useSelector } from 'react-redux';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 
 const Displaylist = () => {
-  const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
+  const items = useSelector  ((state) => [
+    selectFeaturedCampsite(state),
+    selectFeaturedPromotion(state),
+    selectFeaturedPartner(state)
+]);
+console.log('display items:', items);
   return (
     <Row>
       {
